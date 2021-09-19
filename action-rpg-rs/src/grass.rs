ce33c68eb7c6bd042480fbb52680e2cfdb31a393
nejat <nejatoz@gmail.com>
2021-09-19 18:32:43 -0400

    [tech-debt] assume safe if macro needs refactoring
    * see notes

--- action-rpg-rs/src/grass.rs
+++ action-rpg-rs/src/grass.rs
@@ -19,6 +19,7 @@ impl Grass {
     #[export]
     #[allow(non_snake_case)]
     fn _on_HurtBox_area_entered(&mut self, owner: &Node2D, _area: Ref<Area2D>) {
+        // todo: refactor assume_safe_if macro to accept multiple expressions
         load_resource! { scene: PackedScene = "Effects/GrassEffect.tscn" {
             assume_safe_if! { let instance: Node2D = scene.instance(PackedScene::GEN_EDIT_STATE_DISABLED) => {
                 assume_safe_if! { let root = Node::get_tree(&owner) => {
@@ -29,6 +30,17 @@ impl Grass {
                 } }
             } }
         } }
+
+        // todo: example of prior todo refactoring tech debt
+        // load_resource! { scene: PackedScene = "Effects/GrassEffect.tscn" {
+        //     assume_safe_if! {
+        //         let instance = scene.instance(PackedScene::GEN_EDIT_STATE_DISABLED),
+        //         let root = Node::get_tree(&owner),
+        //         let current = root.current_scene() => {
+        //             current.add_child(instance, false);
+        //         }
+        //     }
+        // } }
 
         owner.queue_free();
     }
