31ee9270fb907ee44c811ce5d1ad18ec6fbe14d2
nejat <nejatoz@gmail.com>
2021-09-19 16:01:05 -0400

    [new] added load resource and assume safe if macros

--- action-rpg-rs/src/utils.rs
+++ action-rpg-rs/src/utils.rs
@@ -17,3 +17,53 @@ macro_rules! get_parameter {
         let $var = unsafe { $var.assume_safe() };
     }
 }
+
+#[macro_export]
+macro_rules! load_resource {
+    ($res:ident : $type:ty = $src:literal $code:block) => {
+        if let Some($res) = ResourceLoader::godot_singleton()
+            .load(concat!("res://", $src), stringify!($type), false)
+            .and_then(|res| res.cast::<$type>()) {
+            let $res = unsafe { $res.assume_safe() };
+            $code
+        }
+    };
+    ($res:ident : $type:ty = $src:expr => $code:block) => {
+        if let Some($res) = ResourceLoader::godot_singleton()
+            .load($src, stringify!($type), false)
+            .and_then(|res| res.cast::<$type>())  {
+            let $res = unsafe { $res.assume_safe() };
+            $code
+        }
+    };
+    ($src:literal: $type:ty) => {
+        unsafe {
+            ResourceLoader::godot_singleton()
+                .load(concat!("res://", $src), stringify!($type), false)
+                .and_then(|res| res.cast::<$type>())
+                .assume_safe()
+        }
+    };
+    ($src:expr; $type:ty) => {
+        unsafe {
+            ResourceLoader::godot_singleton()
+                .load($src, stringify!($type), false)
+                .and_then(|res| res.cast::<$type>())
+                .assume_safe()
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! assume_safe_if {
+    (let $var:ident : $type:ty = $src: expr => $code: block) => {
+        if let Some($var) = $src.and_then(|v| { let v = unsafe { v.assume_safe() }; v.cast::<$type>() }) {
+            $code
+        }
+    };
+    (let $var:ident = $src: expr => $code: block) => {
+        if let Some($var) = $src.and_then(|v| { let v = unsafe { v.assume_safe() }; Some(v) }) {
+            $code
+        }
+    };
+}
