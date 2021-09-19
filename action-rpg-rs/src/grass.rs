d9b24aaa8ecd5f6b93b88c1746c9f5075ccfb31d
nejat <nejatoz@gmail.com>
2021-09-19 18:00:46 -0400

    [new] added hurt box and area_entered signal to grass

--- action-rpg-rs/src/grass.rs
+++ action-rpg-rs/src/grass.rs
@@ -17,35 +17,19 @@ impl Grass {
 #[methods]
 impl Grass {
     #[export]
-    fn _process(&mut self, owner: &Node2D, _delta: f32) {
-        let input = Input::godot_singleton();
-
-        if input.is_action_just_pressed("ui_attack") {
-
-            // todo: refactor assume_safe_if macro to accept multiple expressions
-            load_resource! { scene: PackedScene = "Effects/GrassEffect.tscn" {
-                assume_safe_if! { let instance: Node2D = scene.instance(PackedScene::GEN_EDIT_STATE_DISABLED) => {
-                    assume_safe_if! { let root = Node::get_tree(&owner) => {
-                        assume_safe_if! { let current = root.current_scene()  => {
-                            current.add_child(instance, false);
-                            instance.set_global_position(owner.global_position());
-                        } }
+    #[allow(non_snake_case)]
+    fn _on_HurtBox_area_entered(&mut self, owner: &Node2D, _area: Ref<Area2D>) {
+        load_resource! { scene: PackedScene = "Effects/GrassEffect.tscn" {
+            assume_safe_if! { let instance: Node2D = scene.instance(PackedScene::GEN_EDIT_STATE_DISABLED) => {
+                assume_safe_if! { let root = Node::get_tree(&owner) => {
+                    assume_safe_if! { let current = root.current_scene()  => {
+                        current.add_child(instance, false);
+                        instance.set_global_position(owner.global_position());
                     } }
                 } }
             } }
+        } }
 
-            // todo: example of prior todo refactoring tech debt
-            // load_resource! { scene: PackedScene = "Effects/GrassEffect.tscn" {
-            //     assume_safe_if! {
-            //         let instance = scene.instance(PackedScene::GEN_EDIT_STATE_DISABLED),
-            //         let root = Node::get_tree(&owner),
-            //         let current = root.current_scene() => {
-            //             current.add_child(instance, false);
-            //         }
-            //     }
-            // } }
-
-            owner.queue_free();
-        }
+        owner.queue_free();
     }
 }
