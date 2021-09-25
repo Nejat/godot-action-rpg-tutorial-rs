abba4d92ed09fb4157f83dbf51a0864d969b66d6
nejat <nejatoz@gmail.com>
2021-09-25 05:55:50 -0400

    [refactor] preload grass effect scene

--- action-rpg-rs/src/grass.rs
+++ action-rpg-rs/src/grass.rs
@@ -6,29 +6,38 @@ use crate::load_resource;
 
 #[derive(NativeClass)]
 #[inherit(Node2D)]
-pub struct Grass;
+pub struct Grass {
+    effect: Option<Ref<PackedScene>>
+}
 
 impl Grass {
     fn new(_owner: &Node2D) -> Self {
-        Grass
+        Grass { effect: None }
     }
 }
 
 #[methods]
 impl Grass {
     #[export]
+    fn _ready(&mut self, _owner: &Node2D) {
+        load_resource! { scene: PackedScene = "Effects/GrassEffect.tscn" {
+            self.effect = Some(scene.claim())
+        } }
+    }
+
+    #[export]
     #[allow(non_snake_case)]
     fn _on_HurtBox_area_entered(&mut self, owner: &Node2D, _area: Ref<Area2D>) {
-        load_resource! { scene: PackedScene = "Effects/GrassEffect.tscn" {
-            assume_safe! {
-                let instance: Node2D = scene.instance(PackedScene::GEN_EDIT_STATE_DISABLED),
-                let root: SceneTree = Node::get_tree(&owner),
-                let current: Node = root.current_scene() => {
-                    current.add_child(instance, false);
-                    instance.set_global_position(owner.global_position());
-                }
+        let scene = assume_safe!(self.effect);
+
+        assume_safe! {
+            let instance: Node2D = scene.instance(PackedScene::GEN_EDIT_STATE_DISABLED),
+            let root: SceneTree = Node::get_tree(&owner),
+            let current: Node = root.current_scene() => {
+                current.add_child(instance, false);
+                instance.set_global_position(owner.global_position());
             }
-        } }
+        }
 
         owner.queue_free();
     }
